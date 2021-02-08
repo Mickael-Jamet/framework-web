4e048cf27af3906870d2b255b77cd61fd5888146
Mickael <jametmickael1@gmail.com>
2021-02-08 12:58:21 +0100

    utilisation du repository

--- app/controllers/OrgaController.php
+++ app/controllers/OrgaController.php
@@ -3,21 +3,31 @@ namespace controllers;
  use models\Organization;
  use Ubiquity\attributes\items\router\Route;
  use Ubiquity\orm\DAO;
+ use Ubiquity\orm\repositories\ViewRepository;
 
  /**
   * Controller OrgaController
   */
 class OrgaController extends ControllerBase{
+
+    private ViewRepository $repo;
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->repo=new ViewRepository($this, Organization::class);
+    }
+
     #[Route('orga')]
 	public function index(){
-        $orgas=DAO::getAll(Organization::class,"", false);
-		$this->loadView("OrgaController/index.html", ['orgas'=>$orgas]);
+        $this->repo->all("", false);
+		$this->loadView("OrgaController/index.html");
 	}
 
 	#[Route(path: "orga/{idOrga}",name: "orga.getOne")]
 	public function getOne($idOrga){
-		$orga=DAO::getById( Organization::class, $idOrga, ['users.groupes','groupes.users']);
-		$this->loadDefaultView(['orga'=>$orga]);
+		$this->repo->byId( $idOrga, ['users.groupes','groupes.users']);
+		$this->loadDefaultView();
 	}
 
 }
