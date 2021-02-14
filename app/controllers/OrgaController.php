3f59a4da9d3aebfd17057ee174c238c4e30b8d8e
Mickael <jametmickael1@gmail.com>
2021-02-15 00:03:24 +0100

    add, update, delete pas encore terminé

--- app/controllers/OrgaController.php
+++ app/controllers/OrgaController.php
@@ -1,5 +1,6 @@
 <?php
 namespace controllers;
+use Ubiquity\attributes\items\router\Post;
  use models\Organization;
  use Ubiquity\attributes\items\router\Route;
  use Ubiquity\orm\DAO;
@@ -28,6 +29,38 @@ class OrgaController extends ControllerBase{
 	public function getOne($idOrga){
 		$this->repo->byId( $idOrga, ['users.groupes','groupes.users']);
 		$this->loadDefaultView();
+	}
+
+
+	#[Post(path: "orga/add",name: "orga.add")]
+	public function add(){
+        $orga=new Organization();
+        URequest::setValuesToObject($orga);
+        if(DAO::insert($orga)) {
+            console.log("Insertion réussie");
+        }
+        $this->loadView("OrgaController/add.html");
+	}
+
+
+	#[Post(path: "orga/update/{idOrga}",name: "orga.update")]
+	public function update($idOrga){
+        $orga=DAO::getById(Organization::class,$idOrga);
+        URequest::setValuesToObject($orga);
+        if(DAO::update($orga)){
+            console.log("Mise à jour réussie");
+        }
+        $this->loadView("OrgaController/update.html");
+	}
+
+
+	#[Post(path: "orga/delete/{idOrga}",name: "orga.delete")]
+	public function delete($idOrga){
+        $orga=DAO::getById(Organization::class,$idOrga);
+        if(DAO::remove($orga)){
+            console.log("Suppression réussie");
+        }
+        $this->loadView("OrgaController/delete.html");
 	}
 
 }
