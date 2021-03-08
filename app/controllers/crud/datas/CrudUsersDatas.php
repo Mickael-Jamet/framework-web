154e98a407edf98205cd549b964f1307d2ceef73
Mickael <jametmickael1@gmail.com>
2021-03-08 13:19:51 +0100

    modif CrudUsersDatas

--- app/controllers/crud/datas/CrudUsersDatas.php
+++ app/controllers/crud/datas/CrudUsersDatas.php
@@ -8,6 +8,21 @@ use Ubiquity\controllers\crud\CRUDDatas;
 class CrudUsersDatas extends CRUDDatas{
     public function getFieldNames($model)
     {
-        return ['firstname', 'lastname', 'email'];
+        return ['firstname', 'lastname', 'email', 'suspended', 'groups'];
+    }
+
+    public function getFormFieldNames($model, $instance)
+    {
+        return ['firstname', 'lastname', 'email', 'suspended', 'groups'];
+    }
+
+    public function _getInstancesFilter($model)
+    {
+        return parent::_getInstancesFilter($model);
+    }
+
+    public function getManyToManyDatas($fkClass, $instance, $member)
+    {
+        return parent::getManyToManyDatas($fkClass, $instance, $member);
     }
 }
